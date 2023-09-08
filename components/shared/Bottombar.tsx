5a7cb50fd7f1683efae28123033db6139c7d3bf1
Sonia Abhyankar <abhyankar.sonia@gmail.com>
2023-09-08 13:25:31 +0200

    Add layout structure

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+'use client';
+
+import { sidebarLinks } from '@/constants';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import { SignOutButton, SignedIn } from '@clerk/nextjs';
 
 const Bottombar = () => {
-	return <div>Bottombar</div>;
+	const pathname = usePathname();
+	const router = useRouter();
+
+	return (
+		<section className='bottombar'>
+			<div className='bottombar_container'>
+				{sidebarLinks.map((link) => {
+					const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+					return (
+						<Link href={link.route} key={link.label} className={`bottombar_link ${isActive && 'bg-primary-500'}`}>
+							<Image src={link.imgURL} alt={link.label} width={20} height={20} />
+							<p className='text-light-1 text-subtle-medium max-sm:hidden'>{link.label.split(/\s+/)[0]}</p>
+						</Link>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Bottombar;
