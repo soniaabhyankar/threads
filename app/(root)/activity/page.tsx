4c6fd92272f4599fda3ecab36acaab42c8534d31
Sonia Abhyankar <abhyankar.sonia@gmail.com>
2023-09-10 18:42:58 +0200

    Implement the Activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,7 +1,49 @@
+import UserCard from '@/components/cards/UserCard';
+import PostThread from '@/components/forms/PostThread';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { profileTabs } from '@/constants';
+import { fetchUser, fetchUsers, getActivity } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
 const Activity = async () => {
+	const user = await currentUser();
+
+	if (!user) return null;
+
+	const userInfo = await fetchUser(user.id);
+
+	if (!userInfo?.onboarded) redirect('/onboarding');
+
+	const activity = await getActivity(userInfo._id);
+
 	return (
 		<section>
 			<h1 className='head-text mb-10'>Activity</h1>
+
+			<section className='mt-10 flex flex-col gap-5'>
+				{activity.length > 0 ? (
+					<>
+						{activity.map((activity) => (
+							<Link key={activity._id} href={`/thread/${activity.parentId}`}>
+								<article className='activity-card'>
+									<Image src={activity.author.image} alt='profile pic' width={20} height={20} className='rounded-full object-cover' />
+
+									<p className='!text-small-regular text-light-1'>
+										<span className='mr-1 text-primary-500'>{activity.author.name}</span> replied to your thread
+									</p>
+								</article>
+							</Link>
+						))}
+					</>
+				) : (
+					<p className='!text-base-regular text-light-3'>No activity yet</p>
+				)}
+			</section>
 		</section>
 	);
 };
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -127,3 +127,31 @@ export async function fetchUsers({
 		throw new Error(`Error fetching user: ${error.message}`);
 	}
 }
+
+export async function getActivity(userId: string) {
+	try {
+		connectToDB();
+
+		// Find all threads created by the user
+		const userThreads = await Thread.find({ author: userId });
+
+		// Collect all the child thread ids (replies) from the 'children' field
+		const childThreadIds = userThreads.reduce((acc, userThread) => {
+			return acc.concat(userThread.children);
+		}, []);
+
+		// Exclude threads created by current user
+		const replies = await Thread.find({
+			_id: { $in: childThreadIds },
+			author: { $ne: userId },
+		}).populate({
+			path: 'author',
+			model: User,
+			select: 'name image _id',
+		});
+
+		return replies;
+	} catch (error: any) {
+		throw new Error(`Couldn't get activity: ${error.message}`);
+	}
+}
